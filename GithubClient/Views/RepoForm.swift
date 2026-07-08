cb0aabb4b7232a001d18931a3d43ffe796e194f2
jxherrera <josuexavi728@gmail.com>
2026-07-08 16:43:41 -0500

    Laboratorio 11. Creacion de pestañas

---
+++ GithubClient/Views/RepoForm.swift
@@ -0,0 +1,24 @@
+//
+//  RepoForm.swift
+//  GithubClient
+//
+//  Created by Usuario invitado on 8/7/26.
+//
+
+import SwiftUI
+
+struct RepoForm: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Formulario de repositorios")
+            }
+            .navigationTitle("Formulario")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    RepoForm()
+}
